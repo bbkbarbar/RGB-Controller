d0b1659e180d48ba851f38ede8fbb934249ff0eb
bbkbarbar <bbk.barbar@gmail.com>
2017-04-04 20:04:33 +0200

    Skeleton for testing on breadboard

--- RGB-Controller.ino
+++ RGB-Controller.ino
@@ -25,6 +25,8 @@
 #define PWM_OUTPUT_CH4              10
 #define PWM_OUTPUT_CH5              11
 
+#define DELAY_BETWEEN_BRIGHTNESS_CHANGES 10
+
 #define DEFAULT_OUTPUT_VALUE         0  // can set any value in range [0..255] depending on planned useage
 
 #define PWM_CHANNEL_COUNT            6  // based on the physical limitations of Arduino Nano
@@ -52,9 +54,14 @@ void setup() {
     pinMode(pwmChannelMap[i], OUTPUT);
     outputs[i] = DEFAULT_OUTPUT_VALUE;
   }
-  
+
+  outputs[1] = 255;
+  outputs[2] = 1;
+  outputs[3] = 255;
+  setOutputs();
   
 }
+
 
 void setOutputs(){
   unsigned char i;
@@ -65,7 +72,23 @@ void setOutputs(){
 
 //=================================================================================
 
+int brightness = 0;
 
 void loop(){
   // int val = analogRead(analogPin);
+
+  for(brightness = 0; brightness <= 256; brightness++){
+    outputs[0] = brightness;
+    outputs[5] = 255 - brightness;
+    setOutputs();
+    delay(DELAY_BETWEEN_BRIGHTNESS_CHANGES);
+  }
+
+  for(brightness = 255; brightness >= 0; brightness--){
+    outputs[0] = brightness;
+    outputs[5] = 255 - brightness;
+    setOutputs();
+    delay(DELAY_BETWEEN_BRIGHTNESS_CHANGES);
+  }
+
 }
