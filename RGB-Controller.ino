bab3f7a4571691f357fec5051f4a6a47d7da5e2e
bbkbarbar <bbk.barbar@gmail.com>
2017-04-13 14:51:50 +0200

    Add handling of 2 further potmeters to set boundary values of shining

--- RGB-Controller.ino
+++ RGB-Controller.ino
@@ -29,11 +29,15 @@
 #define POT2                               A1
 #define POT3                               A2
 #define POT4                               A3
+#define POT5                               A4
+#define POT6                               A5
 
 #define POT_RED                          POT1
 #define POT_GREEN                        POT2
 #define POT_BLUE                         POT3
 #define POT_SPEED                        POT4
+#define POT_MIN                          POT5
+#define POT_MAX                          POT6
 
 #define RED                                 0
 #define GREEN                               1
@@ -46,7 +50,8 @@
 #define SIDE_2_GREEN                        4
 #define SIDE_2_BLUE                         5
 
-#define DEFAULT_OUTPUT_VALUE                0  // can set any value in range [0..255] depending on planned useage
+#define OFF                                 0
+#define DEFAULT_OUTPUT_VALUE              OFF  // can set any value in range [0..255] depending on planned useage
 
 #define PWM_CHANNEL_COUNT                   6  // based on the physical limitations of Arduino Nano
 #define ADC_MAX                          1023  // max value of 10bit adc intput
@@ -71,6 +76,10 @@ const char pwmChannelMap[PWM_CHANNEL_COUNT] = {
 
 unsigned char outputs[PWM_CHANNEL_COUNT];     // Array for storing values of pwm channels
 
+#define TOP  100
+int MAX = 100;
+int MIN =  10;
+
 //=================================================================================
 //=================================================================================
 
@@ -86,7 +95,6 @@ void readColor(){
   color[BLUE] = map(value, 0, ADC_MAX, 0, PWM_MAX); //Map ADC value to PWM value
 }
 
-
 unsigned int readDelay(){
   int value = analogRead(POT_SPEED);
   if(value == 0){
@@ -95,10 +103,21 @@ unsigned int readDelay(){
   return (value / 4); // delay can be between 1 .. 256
 }
 
+void readBoundaryValues(){
+  int value = analogRead(POT_MIN);
+  MIN = map(value, 0, ADC_MAX, 0, TOP);
 
-void setOutputs(){
-  unsigned char i;
-  for(i=0; i<PWM_CHANNEL_COUNT; i++){
+  value = analogRead(POT_MAX);
+  MAX = map(value, 0, ADC_MAX, 1, TOP);
+
+  if(MIN > MAX){
+    MIN = MAX;
+  }
+}
+
+
+void updateOutputs(){
+  for(unsigned char i=0; i<PWM_CHANNEL_COUNT; i++){
     analogWrite(pwmChannelMap[i], outputs[i]);
   }
 }
@@ -108,8 +127,7 @@ void setOutputs(){
 void setup() {
 
   // sets the pin for PWM outputs
-  unsigned char i;
-  for(i=0; i<PWM_CHANNEL_COUNT; i++){
+  for(unsigned char i=0; i<PWM_CHANNEL_COUNT; i++){
     pinMode(pwmChannelMap[i], OUTPUT);
     outputs[i] = DEFAULT_OUTPUT_VALUE;
   }
@@ -118,21 +136,23 @@ void setup() {
   outputs[SIDE_1_RED]   = color[RED];
   outputs[SIDE_1_GREEN] = color[GREEN];
   outputs[SIDE_1_BLUE]  = color[BLUE];
-  outputs[SIDE_2_RED]   = 0;
-  outputs[SIDE_2_GREEN] = 0;
-  outputs[SIDE_2_BLUE]  = 0;
-  setOutputs();
+  outputs[SIDE_2_RED]   = OFF;
+  outputs[SIDE_2_GREEN] = OFF;
+  outputs[SIDE_2_BLUE]  = OFF;
+  updateOutputs();
   delay(START_DELAY_TO_SHOW_COLOR_IN_MS);
 
   readColor();
-  outputs[SIDE_1_RED]   = 0;
-  outputs[SIDE_1_GREEN] = 0;
-  outputs[SIDE_1_BLUE]  = 0;
+  outputs[SIDE_1_RED]   = OFF;
+  outputs[SIDE_1_GREEN] = OFF;
+  outputs[SIDE_1_BLUE]  = OFF;
   outputs[SIDE_2_RED]   = color[RED];
   outputs[SIDE_2_GREEN] = color[GREEN];
   outputs[SIDE_2_BLUE]  = color[BLUE];
-  setOutputs();
+  updateOutputs();
   delay(START_DELAY_TO_SHOW_COLOR_IN_MS);
+
+  readBoundaryValues();
   
 }
 
@@ -141,10 +161,6 @@ void setup() {
 int factor1 = 0;
 int factor2 = 0;
 
-#define TOP  100
-#define MAX  100
-#define MIN   10
-
 //#define COLOR_FACTOR_OF_GREEN     0.3
 //#define COLOR_FACTOR_OF_BLUE      0.4
 
@@ -152,8 +168,8 @@ int factor2 = 0;
 void loop(){
 
   readColor();
-  
-  // int val;
+  readBoundaryValues();
+
   for(factor1 = MIN, factor2 = MAX; factor1 < MAX; factor1++, factor2--){
     outputs[SIDE_1_RED]   = (factor1 * color[RED]  ) / TOP;
     outputs[SIDE_1_GREEN] = (factor1 * color[GREEN]) / TOP;
@@ -161,7 +177,7 @@ void loop(){
     outputs[SIDE_2_RED]   = (factor2 * color[RED]  ) / TOP;
     outputs[SIDE_2_GREEN] = (factor2 * color[GREEN]) / TOP;
     outputs[SIDE_2_BLUE]  = (factor2 * color[BLUE] ) / TOP;
-    setOutputs();
+    updateOutputs();
     delay(readDelay());
   }
 
@@ -172,9 +188,10 @@ void loop(){
     outputs[SIDE_2_RED]   = (factor2 * color[RED]  ) / TOP;
     outputs[SIDE_2_GREEN] = (factor2 * color[GREEN]) / TOP;
     outputs[SIDE_2_BLUE]  = (factor2 * color[BLUE] ) / TOP;
-    setOutputs();
+    updateOutputs();
     delay(readDelay());
   }
 
 }
+
 
